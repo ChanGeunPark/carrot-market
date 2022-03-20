37b98873d98286339932ba0296b913f8077235e5
ChanGeunPark <design795@naver.com>
2022-03-20 20:35:05 +0900

    finish authentication

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,16 +1,15 @@
-import {withIronSessionApiRoute} from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from '@libs/server/client';
-
+import { withApiSession } from '@libs/server/withSession';
 
 async function handler(req : NextApiRequest, res : NextApiResponse<ResponseType>) {
 //클라이언트 핸드러러엔 두개의 prop을 갖는데 한개는 데이터를 보내느것 한개는 데이터를 받는것이다.
 //NextApiRequest를 ctrl 클릭했을때 옵션을 보여준다.
 
 const { token } = req.body;
-
-const exists = await client.token.findUnique({where:{
+console.log(token);
+const foundToken = await client.token.findUnique({where:{
     payload:token,
   },
   //include:{user:true},
@@ -18,21 +17,22 @@ const exists = await client.token.findUnique({where:{
 //여기서 받은 token을 payload로 가지는 Token을 찾는거다.
 //inclued를 사용하려면 우리가 정의한 관계가 필요하다. prisma schima 에 token에 user랑 관계를 한것이 필요함
 
-if (!exists) res.status(404).end();
-console.log(exists);
-
+if (!foundToken) return res.status(404).end();
 //이 토근을 가진 유저를 찾을거다. 만약 찾는다면 유저 정보를 req.session.user에 담을것이다.
 req.session.user = {
-  id: exists?.userId
+  id: foundToken.userId
 }
 await req.session.save();
-res.status(200).end();
+await client.token.deleteMany({
+  where:{
+    userId: foundToken.userId,
+  }
+});//토큰이 찾아서 사용되면 여기서 지워줄것이다.
+
+res.json({ok:true});
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler),{
-  cookieName:"carrotsession",
-  password:"6546534132134685432121321231321321324324654654631633545646541654165464612313543132434535431654"
-});
+export default withApiSession(withHandler("POST", handler));
 //ironsession은 설정을 해줘야한다. 우선 cookieName을 설정해줘야한다, 두번째는 password를 설정해주는건데 이건 쿠키를 암호화하는데 쓰일거다
 
 //만약 누군가 이 password를 안다면 쿠키를 복호화해 가짜 쿠키를 보낼 수도 있다.
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from 'react-hook-form';
 import { cls } from '@libs/client/utils';
 import Button from '@component/button';
 import Input from "@component/input"
 import useMutation from '@libs/client/useMutation';
+import { useRouter } from 'next/router';
 
 interface EnterForm {
   email?:"string",
@@ -50,7 +51,14 @@ export default function Enter() {
     confirmToken(validForm);
   }
 
+  const router = useRouter();
+  useEffect(()=>{
+    if(tokenData?.ok){
+      router.push("/");
+    }
+  },[tokenData, router])
 
+  
   return (
     <div className='mt-16 px-4'>
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
