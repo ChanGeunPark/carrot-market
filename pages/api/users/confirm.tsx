cc03e5557de09ed799a7f67a1a9150913117a4bc
ChanGeunPark <design795@naver.com>
2022-03-20 20:54:51 +0900

    authentication

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -16,15 +16,21 @@ const foundToken = await client.token.findUnique({where:{
 //여기서 받은 token을 payload로 가지는 Token을 찾는거다.
 //inclued를 사용하려면 우리가 정의한 관계가 필요하다. prisma schima 에 token에 user랑 관계를 한것이 필요함
 
-if (!exists) return res.status(404).end();
-console.log(exists);
+if (!foundToken) return res.status(404).end();
 //이 토근을 가진 유저를 찾을거다. 만약 찾는다면 유저 정보를 req.session.user에 담을것이다.
 req.session.user = {
-  id: exists.userId
-}//타임스크립트는 여기서끝난다는걸 모르기때문에 상단res 옆 return을 해줘야한다.
+  id: foundToken.userId
+}
 await req.session.save();
-res.status(200).end();
-}//유저가 어떤 내용을 가지는지 정의해줘야함
+await client.token.deleteMany({
+  where:{
+    userId: foundToken.userId,
+  }
+});//토큰이 찾아서 사용되면 여기서 지워줄것이다.
+
+res.json({ok:true});
+}
+
 export default withApiSession(withHandler("POST", handler));
 //ironsession은 설정을 해줘야한다. 우선 cookieName을 설정해줘야한다, 두번째는 password를 설정해주는건데 이건 쿠키를 암호화하는데 쓰일거다
 
@@ -33,4 +39,4 @@ export default withApiSession(withHandler("POST", handler));
 
 //nextjs는 export default를 안해주면 req, res를 받아오지 않는다.
 
-//9.3 token logic
+//9.3 token logic2
